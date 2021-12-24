064e2005e3377093383b2a0c72fa50fc60b2bfce
kebrahimpour <k1.ebrahimpour@gmail.com>
2021-12-24 14:26:07 +0100

    remove vnc, set SDK PATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-SNUM=$(echo $DISPLAY | sed 's/:\([0-9][0-9]*\)/\1/')
-xvfb-run -n $SNUM -s "-screen 0 1024x768x24" -f ~/.Xauthority openbox-session &
-sleep 1
-x11vnc -display $DISPLAY -usepw -forever -quiet &
+export ZEPHYR_SDK_INSTALL_DIR=/opt/toolchains/zephyr-sdk-0.13.2
 
 exec "$@"
